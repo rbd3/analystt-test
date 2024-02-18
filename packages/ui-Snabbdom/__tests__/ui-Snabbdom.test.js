f403bd8a929752e2826b8c8d733ea70bf896f4f1
rbd3 <rabedesanaandry01@gmail.com>
2024-02-19 02:23:39 +0300

    Setup project

--- packages/ui-Snabbdom/__tests__/ui-Snabbdom.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const uiSnabbdom = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(uiSnabbdom(), 'Hello from uiSnabbdom');
-console.info('uiSnabbdom tests passed');
--- packages/ui-Snabbdom/lib/ui-Snabbdom.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = uiSnabbdom;
-
-function uiSnabbdom() {
-  return 'Hello from uiSnabbdom';
-}
